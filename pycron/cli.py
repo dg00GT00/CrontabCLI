6ba0848ec073339a9609797454a9f41894351459
Gledson Duarte <duarte.gledson@gmail.com>
2021-03-20 11:13:28 -0300

    Added --module, -m switches to the cli and theirs respective help messages

--- pycron/cli.py
+++ pycron/cli.py
@@ -4,10 +4,20 @@ import argparse
 from python_crontab.cron_argparser import MainMediatorFuncs, SubMediatorFuncs
 from python_crontab.pycron_enum import SubPyCron, PyCron
 
+args_tuple = (
+    "int",
+    "(str, [(str, str) case --module, -m specified])"
+)
+
+increment_help = """
+    [Case --module, -m specified, this switch should received the 
+    absolute python's module path location and the python's module name]
+"""
+
 parser = argparse.ArgumentParser(
-    prog="pycron.cli.py",
+    prog="pycron.cli",
     description="""
-    Sets the location of the JobFinders project 
+    Sets the location of a Python project 
     entry point and specifies the interval time
     that the script will be ran
     """)
@@ -18,43 +28,44 @@ update_args = update_parser.add_parser("update")
 
 update_args.add_argument(SubPyCron.OLD.build_args(),
                          action=SubMediatorFuncs,
-                         nargs=2,
+                         nargs=len(args_tuple),
                          required=True,
-                         metavar=("int", "str"),
-                         help="The interval in minutes and python script path to update")
+                         metavar=args_tuple,
+                         help=f"""The interval in minutes and python script path to update. {increment_help}""")
 
 update_args.add_argument(SubPyCron.NEW.build_args(),
                          action=SubMediatorFuncs,
-                         nargs=2,
+                         nargs=len(args_tuple),
                          required=True,
-                         metavar=("int", "str"),
-                         help="The interval in minutes and python script path to set the update")
+                         metavar=args_tuple,
+                         help=f"""The interval in minutes and python script path to update. {increment_help}""")
+
+parser.add_argument(PyCron.MODULE.build_args(), "-m",
+                    action="store_true",
+                    help="""Inserts a Python module instead of a Python script""")
 
 group = parser.add_mutually_exclusive_group()
 
 group.add_argument(PyCron.INIT.build_args(),
                    action=MainMediatorFuncs,
-                   nargs=2,
-                   metavar=("int", "str"),
-                   help=""" Put an entry to crontab file with the 
-                    specified interval in minutes and
-                    the path to python script """)
+                   nargs=len(args_tuple),
+                   metavar=args_tuple,
+                   help=f""" Put an entry to crontab file with the 
+                    specified interval in minutes and the path to python script. {increment_help}""")
 
 group.add_argument(PyCron.INSERT.build_args(),
                    action=MainMediatorFuncs,
-                   nargs=2,
-                   metavar=("int", "str"),
-                   help=""" Inserts a new entry at crontab file with the 
-                    specified interval in minutes and
-                    the path to python script """)
+                   nargs=len(args_tuple),
+                   metavar=args_tuple,
+                   help=f""" Inserts a new entry at crontab file with the 
+                    specified interval in minutes and the path to python script. {increment_help}""")
 
 group.add_argument(PyCron.DELETE.build_args(),
                    action=MainMediatorFuncs,
-                   nargs=2,
-                   metavar=("int", "str"),
-                   help=""" Removes an entry from crontab file with the 
-                    specified interval in minutes and
-                    the path to python script """)
+                   nargs=len(args_tuple),
+                   metavar=args_tuple,
+                   help=f""" Removes an entry from crontab file with the 
+                    specified interval in minutes and the path to python script. {increment_help}""")
 
 parser.add_argument(PyCron.PY.build_args(),
                     action=MainMediatorFuncs,
