44cf7e8fc152f37cb9378783b081761c671ce462
Gledson Duarte <duarte.gledson@gmail.com>
2021-03-20 20:47:02 -0300

    Added a custom exception to when a python module is not specified

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -3,7 +3,7 @@ class MinOutOfRangeException(Exception):
     Exception to crontab minutes range
     """
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "The minutes field must be encompassed from 0 up to 59"
 
 
@@ -12,5 +12,14 @@ class NoCronEntryFound(Exception):
     Exception to crontab minutes range
     """
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "The specified cron entry was not found"
+
+
+class NoPyModuleFound(Exception):
+    """
+    Exception when Python module is not found
+    """
+
+    def __str__(self) -> str:
+        return "When --module, -m switch specified, the python module path and python module name should be determined"
